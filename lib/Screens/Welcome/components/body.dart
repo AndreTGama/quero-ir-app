bdfe3c3ba5545c0e8f6e635db4636b3d5d25b627
AndreTGama <andre_gama789@hotmail.com>
2023-03-12 16:58:54 -0300

    add button to login without onPress

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:quero_ir_app/Components/background.dart';
+import 'package:quero_ir_app/constants.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
+    
+    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      backgroundColor: kPrimaryColor,
+    );
     return Background(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -20,6 +25,13 @@ class Body extends StatelessWidget {
           SvgPicture.asset(
             "assets/icons/chat.svg",
             height: size.height * 0.45,
+          ),
+          TextButton(
+            style: raisedButtonStyle, 
+            onPressed: (){
+
+            },
+            child: const Text("LOGIN"),
           )
         ],
       ),
